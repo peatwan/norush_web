68bd424b51af22c71432c1fe9b2b6e44aff0cfe0
xiongdi94 <xiongdi94@outlook.com>
2017-11-24 23:15:43 +0800

    init

---
+++ flight/flask_airline.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,request,Response
+from flight import app
+#app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+@app.route('/airline',methods=['GET','POST'])
+def process():
+
+
+    return render_template('index.html')
+@app.route('/graph',methods=['GET','POST'])
+def show():
+    print('show got')
+    a=open('D:\\MSBD5001\\proj\\grpahs\\time1.png','rb')
+
+    data=a.read()
+    return Response(data)
+
+
+@app.route('/search',methods=['GET','POST'])
+def search():
+    data=request.form
+    data1=dict(data)
+    print(type(data1['date']))
+    #print(request.form["date"])
+    return Response('hello')
+
+
+'''
+if __name__ == '__main__':
+    app.run()
+'''
---
+++ flight/templates/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	/* This code is executed after the DOM has been completely loaded */
+
+	/* Changing thedefault easing effect - will affect the slideUp/slideDown methods: */
+	$.easing.def = "easeOutBounce";
+
+	/* Binding a click event handler to the links: */
+	$('li.button a').click(function(e){
+	
+		/* Finding the drop down list that corresponds to the current section: */
+		var dropDown = $(this).parent().next();
+		
+		/* Closing all other drop down sections, except the current one */
+		$('.dropdown').not(dropDown).slideUp('slow');
+		dropDown.slideToggle('slow');
+		
+		/* Preventing the default event (which would be to navigate the browser to the link's address) */
+		e.preventDefault();
+	})
+
+	var test1 = $(".date_btn");
+	test1[0].addEventListener("click", function () {
+		alert($(".tt-input")[0].value);
+		alert($("input.book_date")[0].value);
+	})
+
+	$("#submitButton").onclick = function () {
+		alert("2");
+	}
+
+});
