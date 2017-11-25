c0fdc737226edda420dda1d88e950f9cbaf91a77
xiongdi94 <xiongdi94@outlook.com>
2017-11-26 02:22:25 +0800

    update server

--- flight/flask_airline.py
+++ flight/flask_airline.py
@@ -1,15 +1,16 @@
 from flask import Flask,render_template,request,Response
+import socket
+import json
 from flight import app
-
 #app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 @app.route('/airline',methods=['GET','POST'])
 def process():
-
 
     return render_template('index.html')
 @app.route('/graph',methods=['GET','POST'])
@@ -25,12 +26,35 @@ def show():
 def search():
     data=request.form
     data1=dict(data)
-    print(type(data1['date']))
-    #print(request.form["date"])
-    return Response('hello')
+    time=data1['time'][0]
+
+    origin=data1['origin'][0]
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.connect(('23.102.232.105', 9999))
 
 
-'''
-if __name__ == '__main__':
-    app.run()
-'''
+    msg={'method':'get','time':time,'origin':origin}
+    msg=json.dumps(msg)
+    s.send(str.encode(msg))
+
+    print('waiting for response from the spark server')
+
+    res=s.recv(1024)
+
+    res=bytes.decode(res)
+    if res is not None:
+        print(res)
+        s.send(str.encode('done'))
+
+
+
+
+    return Response(res)
+
+
+
+
+#if __name__ == '__main__':
+#    app.run()
